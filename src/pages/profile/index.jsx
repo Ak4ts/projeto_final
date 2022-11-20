ec5c77e7f8a707f11961758af5104e85b664fee0
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-20 15:33:03 -0300

    test

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -4,9 +4,47 @@ import "./style.css";
 
 function Profile() {
   return (
-    <div>
-      
+    <>
+<div class="box-form">
+  <div class="left">
+  <div class="overlay">      
+    <h1 data-text="Frien">Friendo</h1>
+  </div>
+
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+    <div>s</div>
+
     </div>
+ 
+  </div>
+
+    
+    </>
   )
 }
 
