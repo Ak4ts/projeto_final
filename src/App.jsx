76ccbe01172e7d7b6b646d7694923e857eed1279
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-20 14:37:14 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './pages/home'
+import LandingPage from "./pages/landing-page"
 import Login from './pages/login-page'
 import Sign_up from './pages/sign-up-page'
+import RecoverAccount from './pages/recover-account'
+import Profile from "./pages/profile"
+import AccountSettings from "./pages/account-settings"
+import Cards from "./pages/cards"
+import Chats from "./pages/chats"
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/settings' element={<AccountSettings/>}/>
+          <Route path='/cards' element={<Cards/>}/>
+          <Route path='/chats' element={<Chats/>}/>
+          <Route path='/recover-account' element={<RecoverAccount/>}/>
+          <Route path='/landing-page' element={<LandingPage/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/sign-up' element={<Sign_up/>}/>
         </Routes>
