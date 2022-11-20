1b76f1a957ddcd849c1d938e4047e479efb6b48c
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-20 10:57:15 -0300

    create first routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
-import "./App.css";
-import luis from "./img/luis.jpeg"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from './pages/home'
+import Login from './pages/login-page'
+import Sign_up from './pages/sign-up-page'
 function App() {
   return (
-<div class="login-card">
-  <div class="login-card-content">
-    <div class="header">
-      <div class="logo">
-        <div><img src={luis} alt="luis" height="400" width="200"/></div>
-      </div>
-      <h2>Frien<span class="highlight">do</span></h2>
-      <h3>Making friends has never been this easy!</h3>
-    </div>
-    <div class="form">
-      <div class="form-field username">
-        <div class="icon">
-          <i class="far fa-user"></i>
-        </div>
-        <input type="text" placeholder="Username"/>
-      </div>
-      <div class="form-field password">
-        <div class="icon">
-          <i class="fas fa-lock"></i>
-        </div>
-        <input type="password" placeholder="Password"/>
-      </div>
-
-      <button type="submit">
-        Login
-      </button>
-      <div>
-        Don't have an account? <a href="">Sign Up Now</a>
-      </div>
-    </div>
-  </div>
-  <div class="login-card-footer">
-    <a href="">Forgot password?</a>
-  </div>
-</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/sign-up' element={<Sign_up/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
