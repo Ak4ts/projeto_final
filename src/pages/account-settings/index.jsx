f5f7905406ee08f00d0009bda96b08973fc11006
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-23 11:27:32 -0300

    Update index.jsx

--- src/pages/account-settings/index.jsx
+++ src/pages/account-settings/index.jsx
@@ -1,30 +1,60 @@
 import React from "react";
-import "./style.css";
-
+import './style.css'
 
 function Profile() {
   return (
-    <>
-      <div className="left">
-        <ul>
-          <li><a href="/"><i className="fas fa-home"></i></a></li>
-          <li><a href="/settings"><i className="fas fa-cogs"></i></a></li>
-          <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
-          <li><a href="/chat"><i className='far fa-comment-dots'></i></a></li>
-          <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
-        </ul>
-      </div>
-      <div className="right">
-        <div className="box-form">
-          <div className="overlay friendo-profile-title">      
-          <h1 data-text="Friend">Friendo</h1>
-          </div>
-          <div className="profile-settings">
-            <h1>Profile Settings</h1>
-          </div>
+  <div>
+    <div className="left">
+        <div className="list">
+          <ul>
+            <li><a href="/"><i className="fas fa-home"></i></a></li>
+            <li><a href="/settings"><i className="fas fa-cogs"></i></a></li>
+            <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
+            <li><a href="/chat"><i className='far fa-comment-dots'></i></a></li>
+            <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
+          </ul>
         </div>
       </div>
-    </>
+      <div className="right">
+    <div class="row">
+      <div class="col-md-12">
+      <form className="changes">
+        <h1 className="account">Account Settings</h1>
+        
+        <fieldset>
+          
+          <legend><span class="number">1</span> Your Basic Info</legend>
+        
+          <label for="name">Name:</label>
+          <input type="text" id="name" className="account" name="user_name"/>
+        
+          <label for="email">Email:</label>
+          <input type="email" id="mail" className="account" name="user_email"/>
+       
+          <label for="password">Password:</label>
+          <input type="password" className="account" id="password" name="user_password"/>
+          
+        </fieldset>
+        <fieldset>  
+        
+          <legend><span class="number">2</span> Your Profile</legend>
+          
+         <label for="bio">Bio:</label>
+          <textarea id="bio" name="user_bio"></textarea>
+        
+          <label>Interests:</label>
+
+          <textarea id="insterests" name="interests"></textarea>
+          
+         </fieldset>
+       
+        <button className="changes" type="submit">Submit changes</button>
+        
+       </form>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
