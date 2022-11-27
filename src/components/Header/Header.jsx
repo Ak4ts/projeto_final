0e4421b40bd811a35f78169e2e29f22acf9f7f0f
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-27 00:12:03 -0300

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css"
+function Header() {
+  return (
+    <>
+        <ul>
+          <li><a href="/"><i className="fas fa-home"></i></a></li>
+          <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
+          <li><a href="/chat"><i className='far fa-comment-dots'></i></a></li>
+          <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
+          <li><a href=""><i className="fas fa-map-marker-alt"></i></a></li>
+          <li><a href="/settings"><i className="fas fa-cogs"></i></a></li>
+
+        </ul>
+    </>
+  )
+}
+
+export default Header;
