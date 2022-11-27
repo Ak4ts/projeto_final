0dc438bd072ee4156995110d144c9d89bb1a63ae
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-27 18:20:50 -0300

    button effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
           <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
           <li><a href="/chat"><i className='far fa-comment-dots'></i></a></li>
           <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
-          <li><a href=""><i className="fas fa-map-marker-alt"></i></a></li>
+          <li><a href="/locations"><i className="fas fa-map-marker-alt"></i></a></li>
           <li><a href="/settings"><i className="fas fa-cogs"></i></a></li>
 
         </ul>
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 import "./style.css";
 
 
@@ -6,15 +7,7 @@ function Profile() {
   return (
     <>
       <div className="left">
-        <div className="list">
-          <ul>
-            <li><a href="/"><i className="fas fa-home"></i></a></li>
-            <li><a href="/settings"><i className="fas fa-cogs"></i></a></li>
-            <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
-            <li><a href="/chat"><i className='far fa-comment-dots'></i></a></li>
-            <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
-          </ul>
-        </div>
+        <Header/>
       </div>
       <div className="right">
         <div className="box-form">
@@ -22,7 +15,7 @@ function Profile() {
           <h1 data-text="Friend">Friendo</h1>
           </div>
           <div className="profile-settings">
-            <h1>Profile Settings</h1>
+            <h1>Profile</h1>
           </div>
         </div>
       </div>
