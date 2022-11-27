d6da76f30d26b2df9b2c4ec645050b0858530490
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-27 00:13:14 -0300

    Update index.jsx

--- src/pages/chats/index.jsx
+++ src/pages/chats/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './style.css'
-
+import Header from "../../components/Header/Header";
 
 function Chats() {
   return (
-    <div></div>
+    <>
+    <div className="left">
+      <Header/>
+    </div>
+    <div className="right">
+      HELLOOO
+    </div>
+    </>
   )
 }
 
