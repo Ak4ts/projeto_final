e5f3e268b22750f0d3af09e5d241cc29f7dc80f6
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-29 11:43:24 -0300

    Update index.jsx

--- src/pages/recover-account/index.jsx
+++ src/pages/recover-account/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./style.css";
-
+import Header from "../../components/Header/Header";
 
 function RecoverAccount() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="left">
+        <Header/>
+      </div>
+      <div className="right">
+        <div className="cont">
+          <h2>Recover Account</h2>
+          <fieldset>
+          <label for="email">Email:</label>
+          <input type="email" id="mail" className="account" name="user_email"/>
+          <div className="place-btns">
+            <button className="reset" type="submit">Reset password</button>
+          </div>
+
+          </fieldset>
+        </div>
+      </div>
+    </>
   )
 }
 
