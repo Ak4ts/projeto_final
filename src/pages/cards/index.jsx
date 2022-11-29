a8488a86c5d99d6d44e1eb75cd3c87d766dd890a
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-29 11:43:19 -0300

    Update index.jsx

--- src/pages/cards/index.jsx
+++ src/pages/cards/index.jsx
@@ -7,6 +7,10 @@ import coffee from "../../img/cafe.jpg"
 import felipe from "../../img/felipe.png"
 import maria from "../../img/friends.jpg"
 import friendos from "../../img/friendos.jpeg"
+import luid from "../../img/luid.jpeg"
+import gabriel from "../../img/gabs.jpeg"
+import john from "../../img/guy1.jpg"
+import juliana from "../../img/woman1.jpg"
 import Header from "../../components/Header/Header";
 function Cards() {
   return (
@@ -21,12 +25,15 @@ function Cards() {
           </div>
           <h1 className="card-title">Meet new people!</h1>
           <div className="cards">
-            <Card image={luis} name="Luis Gabriel" age='14' description= "I like going places and  meeting new people!" interests="Surfing, Skiing, Golfing, Surfing, Skiing, Golfing, Surfing, Skiing, Golfing,Surfing, Skiing, Golfing,Surfing, Skiing, Golfing" />
-            <Card image={andre} name="Andre" age='53' description= "I like going places and  meeting new people!" interests="Surfing, Skiing, Golfing, Surfing, Skiing, Golfing, Surfing, Skiing, Golfing,Surfing, Skiing, Golfing,Surfing, Skiing, Golfing"/>
-            <Card image={felipe} name="Felipe" age='17' description= "I like going places and  meeting new people!" interests="Gaming, watching movies and listening to music"/>
-            <Card image={friendos} name="João Gabriel" age='12' description= "I like going places and  meeting new people!" interests="Surfing, Skiing, Golfing, Surfing, Skiing, Golfing, Surfing, Skiing, Golfing,Surfing, Skiing, Golfing,Surfing, Skiing, Golfing"/>
-            <Card image={maria} name="Maria" age='19' description= "I like going places and  meeting new people!" interests="Surfing, Skiing, Golfing, Surfing, Skiing, Golfing, Surfing, Skiing, Golfing,Surfing, Skiing, Golfing,Surfing, Skiing, Golfing"/>
-            <Card image={coffee} name="Anna" age='25' description= "I like going places and meeting new people!" interests="Astrology, coffee"/>
+            <Card image={luis} name="Luis Gabriel" age='14' description= "Love animals and plants. hate ugly people. Love u" interests="Plants, animals and astrology" />
+            <Card image={andre} name="Andre" age='53' description= "giga chad 🗿🌿 guitarrista 🎸emo 💀 🖤 " interests="Rock e Emos"/>
+            <Card image={felipe} name="Felipe" age='47' description= "I like going places and  meeting new people!" interests="Gaming, watching movies and listening to music"/>
+            <Card image={friendos} name="João Gabriel" age='22' description= "I like going places and  meeting new people!" interests="Surfing"/>
+            <Card image={maria} name="Maria" age='19' description= "I like going places and  meeting new people!" interests="Books, mountains"/>
+            <Card image={gabriel} name="Gabriel" age='65' description= "Música: rock, lugar: tranquilo, mulher: loira" interests="Música"/>
+            <Card image={luid} name="Luide" age='25' description= "Um lobo solitário que gosta de bater nos outros" interests="Coisas de nerd, lobos"/>
+            <Card image={john} name="John" age='21' description= "Boring" interests="painting"/>
+            <Card image={juliana} name="Juliana" age='25' description= ":)" interests="Coffe, parks"/>
           </div>
           
         </div>
