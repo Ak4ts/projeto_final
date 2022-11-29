5a2c06cc282d84516425f95c8a1006cd06abadc5
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-29 11:43:07 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ function Header() {
     <>
         <ul>
           <li><a href="/"><i className="fas fa-home"></i></a></li>
-          <li><a href="/profile"><i className="fas fa-user-circle"></i></a></li>
           <li><a href="/notifications"><i className='far fa-comment-dots'></i></a></li>
           <li><a href="/cards"><i className="fas fa-heart"></i></a></li>
           <li><a href="/locations"><i className="fas fa-map-marker-alt"></i></a></li>
