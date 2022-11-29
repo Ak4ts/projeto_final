4a2ef2a9caec955a9c4712c8dba0e69cf7511119
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-11-29 11:43:21 -0300

    Update index.jsx

--- src/pages/notifications/index.jsx
+++ src/pages/notifications/index.jsx
@@ -5,6 +5,7 @@ import guy1 from "../../img/guy1.jpg"
 import woman1 from "../../img/helen.jpg"
 import woman from "../../img/woman1.jpg"
 import woman2 from "../../img/laura.jpg"
+import gabriel from "../../img/gabs.jpeg"
 import "./style.css";
 
 
@@ -22,10 +23,11 @@ function Notifications() {
           <div className="profile-settings">
             <h1>Notifications</h1>
             <div class="ui feed">
-              <FeedItem img={guy1} user="John R." desc="added you as a friend" date="Just Now" likes="4 likes" txt="Hey friend"/>
-              <FeedItem img={woman2} user="Lidia H." desc="sent you a friend request" date="3h ago" likes="4" txt="I saw our interests are similar"/>
-              <FeedItem img={woman} user="Juliana J." desc="invited you to go to a restaurant" date="13h ago" likes="4" txt="Hey! Let's meet"/>
-              <FeedItem img={woman1} user="Juliana J." desc="invited you to go to the park" date="4d ago" likes="4" txt="Wanna meet at 4pm?"/>
+              <FeedItem img={guy1} user="John R." desc="added you as a friend" date="Just Now" txt="Hey friend"/>
+              <FeedItem img={woman2} user="Lidia H." desc="sent you a friend request" date="3h ago" txt="I saw our interests are similar"/>
+              <FeedItem img={woman} user="Juliana J." desc="invited you to go to a restaurant" date="13h ago" txt="Hey! Let's meet"/>
+              <FeedItem img={woman1} user="Angela G." desc="invited you to go to the park" date="4d ago" txt="Wanna meet at 4pm?"/>
+              <FeedItem img={gabriel} user="Gabriel B." desc="messaged you" date="6d ago" txt="nice to see you yesterday"/>
             </div>
           </div>
         </div>
